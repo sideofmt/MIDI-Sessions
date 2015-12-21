70b17e5a3cce50a00bdb23c2b749fa0d79f5be01
sideofmt <mtmt.garireo.39440@gmail.com>
2015-12-22 00:26:12 +0900

    feature / メインフォームのメニューバー追加

--- MIDI-Sessions/MIDI-Sessions/Form1.Designer.cs
+++ MIDI-Sessions/MIDI-Sessions/Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.動作ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.接続ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.通信の設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.動作ToolStripMenuItem,
+            this.接続ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 動作ToolStripMenuItem
+            // 
+            this.動作ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.終了ToolStripMenuItem});
+            this.動作ToolStripMenuItem.Name = "動作ToolStripMenuItem";
+            this.動作ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.動作ToolStripMenuItem.Text = "動作";
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.終了ToolStripMenuItem.Text = "終了";
+            this.終了ToolStripMenuItem.Click += new System.EventHandler(this.終了ToolStripMenuItem_Click);
+            // 
+            // 接続ToolStripMenuItem
+            // 
+            this.接続ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.通信の設定ToolStripMenuItem});
+            this.接続ToolStripMenuItem.Name = "接続ToolStripMenuItem";
+            this.接続ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.接続ToolStripMenuItem.Text = "通信";
+            // 
+            // 通信の設定ToolStripMenuItem
+            // 
+            this.通信の設定ToolStripMenuItem.Name = "通信の設定ToolStripMenuItem";
+            this.通信の設定ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.通信の設定ToolStripMenuItem.Text = "通信の設定";
+            this.通信の設定ToolStripMenuItem.Click += new System.EventHandler(this.通信の設定ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 動作ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 接続ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 通信の設定ToolStripMenuItem;
     }
 }
 
--- MIDI-Sessions/MIDI-Sessions/Form1.cs
+++ MIDI-Sessions/MIDI-Sessions/Form1.cs
@@ -12,9 +12,41 @@ namespace MIDI_Sessions
 {
     public partial class Form1 : Form
     {
+        DebugMonitor debugMonitor;
+
         public Form1()
         {
             InitializeComponent();
+            //Form2クラスのインスタンスを作成する
+            debugMonitor = new DebugMonitor();
+            //Form2を表示する
+            //ここではモードレスフォームとして表示する
+            debugMonitor.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            debugMonitor.setAddText("メインフォームのロードが完了しました");
+        }
+
+        private void 終了ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //自分自身のフォームがメインフォームの時、
+            //自分自身のフォームをCloseメソッドで閉じると、アプリケーションが終了する
+            debugMonitor.setAddText("アプリケーションを終了します");
+            this.Close();
+        }
+
+        private void 通信の設定ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Form2クラスのインスタンスを作成する
+            AccessForm accessForm = new AccessForm();
+            //Form2を表示する
+            debugMonitor.setAddText("通信設定ダイアログを開きます");
+            accessForm.ShowDialog(this);
+            debugMonitor.setAddText("通信設定ダイアログを閉じます");
+            //フォームが必要なくなったところで、Disposeを呼び出す
+            accessForm.Dispose();
         }
     }
 }
