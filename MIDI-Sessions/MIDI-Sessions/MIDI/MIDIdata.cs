35eab3e8008dfad7aff01962625f450a4c0412d7
Keita-Watanabe <kt10k31t@gmail.com>
2015-12-22 15:58:15 +0900

    MidiDataの追加

--- MIDI-Sessions/MIDI-Sessions/MIDI/MIDIdata.cs
+++ MIDI-Sessions/MIDI-Sessions/MIDI/MIDIdata.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Midi;
 
-namespace MIDI_Sessions
-{
-    class MIDIdata
-    {
+namespace MIDI_Sessions{
+    class MIDIData{
+        private Channel cha;
+        private Pitch pit;
+        private int velocity;
+        private bool isPushing;
+
+        public Channel Cha {
+            set { this.cha = value; }
+            get { return this.cha; }
+        }
+
+        public Pitch Pit {
+            set { this.pit = value; }
+            get { return this.pit; }
+        }
+
+        public int Velocity {
+            set { this.velocity = value; }
+            get { return this.velocity; }
+        }
+
+        public bool IsPushing {
+            set { this.isPushing = value; }
+            get { return this.isPushing; }
+        }
     }
 }
